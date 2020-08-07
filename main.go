499fbd6f17b33d14c9751514445b7e549cd07594
b3b3r <b3b3rseg@gmail.com>
2020-08-07 16:51:33 +0200

    loop from csv to  generate multiple files

--- main.go
+++ main.go
@@ -21,7 +21,14 @@ func handleErr(err error) {
 
 func main() {
 	outputType := flag.String("type", "pdf", "Output type of certificate")
+	filename := flag.String("file", "students.csv", "CSV filename to parse")
 	flag.Parse()
+
+	if len(*filename) <= 0 {
+		fmt.Println("Filename is not valid")
+		os.Exit(1)
+	}
+
 	var saver cert.Saver
 	var err error
 	switch *outputType {
@@ -33,7 +40,12 @@ func main() {
 		fmt.Printf("Unknow output type, got=%v", *outputType)
 	}
 	handleErr(err)
-	c, err := cert.New("Golang", "John", "2020-08-07")
+	certs, err := cert.ParseCSV(*filename)
 	handleErr(err)
-	saver.Save(*c)
+	for _, cert := range certs {
+		err = saver.Save(*cert)
+		if err != nil {
+			fmt.Println("Could not save file")
+		}
+	}
 }
