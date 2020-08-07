0d8454e3eae8b1fc048c8e843a88333aef45f737
b3b3r <b3b3rseg@gmail.com>
2020-08-07 15:42:13 +0200

    integrate CLI to laucnh bin

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
+//go build
+// ./genCert -type pdf
+
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"udemy/genCert/cert"
+	"udemy/genCert/html"
 	"udemy/genCert/pdf"
 )
 
@@ -15,10 +20,20 @@ func handleErr(err error) {
 }
 
 func main() {
-	c, err := cert.New("Golang", "John", "2020-08-07")
-	handleErr(err)
+	outputType := flag.String("type", "pdf", "Output type of certificate")
+	flag.Parse()
 	var saver cert.Saver
-	saver, err = pdf.New("output")
+	var err error
+	switch *outputType {
+	case "html":
+		saver, err = html.New("output")
+	case "pdf":
+		saver, err = pdf.New("output")
+	default:
+		fmt.Printf("Unknow output type, got=%v", *outputType)
+	}
+	handleErr(err)
+	c, err := cert.New("Golang", "John", "2020-08-07")
 	handleErr(err)
 	saver.Save(*c)
 }
